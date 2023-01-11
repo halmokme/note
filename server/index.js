77631652d475a77fc24bee75d6937dbe7239d48f
halmokme <107684690+halmokme@users.noreply.github.com>
2023-01-11 22:27:46 +0900

    [server] 초기세팅

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("success");
+});
+
+app.listen(4000, () => {
+  console.log("server on!");
+});
